b4eefcc0419d025c39eec45623f894973663042e
Rajat parida <rajat.parida@zalando.de>
2019-01-18 00:36:14 +0100

    Updated Readme, Authentication service, bolt package, internal package

--- wtf.go
+++ wtf.go
@@ -10,11 +10,11 @@ type User struct {
 type Dial struct {
 	ID           int       `json:"Id"`
 	UserID       int       `json:"userId"`
-	status       float64   `json:"status"`
+	Status       float64   `json:"status"`
 	ModifiedTime time.Time `json:"lastModified"`
 }
 
-type UserService interface {
+type Authenticator interface {
 	Authenticate(token string) *User
 }
 
@@ -22,4 +22,13 @@ type DialService interface {
 	CreateDial(dial *Dial) error
 	GetDial(dialID int) (*Dial, error)
 	SetStatus(status int, dialID int) error
+}
+
+type Client interface {
+	Connect() Session
+}
+
+type Session interface {
+	DialService() DialService
+	SetAuthToken(token string)
 }
